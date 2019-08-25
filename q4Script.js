5f7f012738dce5a305779a4237379a383d3909fc
Rijan Chapgain <hrijon.chp@gmail.com>
2019-08-25 14:39:41 +0800

    Taking userInput from input and displaying in text area

---
+++ q4Script.js
@@ -0,0 +1,13 @@
+var emptyArray =[];
+var name;
+
+
+function myFunction(){
+    var name = document.MyForm.name.value; 
+
+    // var x = document.getElementById("input");
+    // emptyArray.push(x);
+   
+}
+
+console.log(name);
